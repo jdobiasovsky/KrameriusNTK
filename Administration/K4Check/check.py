860b41ac55be8f330aa06ba831cac21ccd97982a
jdobiasovsky <jan.dobiasovsky@gmail.com>
2017-06-19 11:26:52 +0200

    idlist now reads from aleph scratch file

--- Administration/K4Check/check.py
+++ Administration/K4Check/check.py
@@ -14,16 +14,21 @@ input("Press ENTER to continue...")
 print("\n\n")
 print("Checking presence in catalog...")
 
-idlist = open("./input/idlist.xml", "r")
-idlist_walk = re.findall("(uuid:.*?)<", idlist.read(), re.DOTALL)
+idlist = open("./input/856_kramerius", "r")
+idlist_walk = re.findall("(search/handle/)(uuid:.*?)(\$\$y)", idlist.read(), re.DOTALL)
+idlist_walk = [x[1] for x in idlist_walk]
+# makes list of uuid identifiers found in 856_kramerius (documents present in catalog)
 print("Total items: ", len(idlist_walk))
 
 missing = open("./missing_in_cat.txt", "w")
 correct = open("./present_in_cat.txt", "w")
+# files that will list all documents present / missing in catalog
 missing_count = 0
 correct_count = 0
+# counters for no. of documents present / missing
 
 for files in glob.glob("./input/fcrepo_export/*.xml"):
+    # iterates through all files in ^path, checks their uuid against idlist, copies to target folders according to result
     file_uuid = re.search("(./input/fcrepo_export/uuid_)(.*?)(.xml)", files)
     uuid = str("uuid:" + file_uuid.group(2))
     if uuid not in idlist_walk:
@@ -43,13 +48,14 @@ correct.close()
 
 print("Verifying metadata structure...")
 
-
 aleph_export = open("./output/aleph_export", "w")
+# file for batch aleph export (scratch/856_kramerius)
 proc_count = 0
 err_count = 0
 disc_count = 0
 
 for file in glob.glob("./input/fcrepo_export/**/*.xml", recursive=True):
+    # iterates through given path, checks model, discards if doesn't math criteria
     data = open(file, "r")
     contents = data.read()
     modelcheckmonograph = re.search("(<dc:type>model:monograph</dc:type>)", contents)
@@ -68,11 +74,12 @@ for file in glob.glob("./input/fcrepo_export/**/*.xml", recursive=True):
             aleph_export.write(sysno + " 85640 L $$uhttp://k4.techlib.cz/search/handle/" + uuid +
                                "$$yDigitalizovaný dokument\n")
             aleph_export.write(sysno + " BAS   L di\n\n")
-
+            # extracts uuid and sysno, writes line into aleph_output
             shutil.move(file, "./output/processed/" + re.search("uuid_.*.xml", file).group(0))
             proc_count += 1
             data.close()
         except AttributeError:
+            # moves aside files that yield error
             shutil.move(file, "./output/error/" + re.search("uuid_.*.xml", file).group(0))
             err_count += 1
             data.close()
@@ -80,6 +87,7 @@ for file in glob.glob("./input/fcrepo_export/**/*.xml", recursive=True):
         shutil.move(file, "./output/discarded/" + re.search("uuid_.*.xml", file).group(0))
         disc_count += 1
         data.close()
+
 
 print("Done!")
 print("\n\n----------------------------------------------------------\n")
